8175948e81c87d4d0581efb12231a50b3ea30561
ZHEQIUSHUI <qiushui.zhe@gmail.com>
2024-01-03 12:24:52 +0800

    add print json format log for ax-pipeline

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,56 @@ struct Object
     cv::Rect2f rect;
 };
 
+bool _file_exist(const std::string &path)
+{
+    auto flag = false;
+
+    std::fstream fs(path, std::ios::in | std::ios::binary);
+    flag = fs.is_open();
+    fs.close();
+
+    return flag;
+}
+
+bool _file_read(const std::string &path, std::vector<char> &data)
+{
+    std::fstream fs(path, std::ios::in | std::ios::binary);
+
+    if (!fs.is_open())
+    {
+        return false;
+    }
+
+    fs.seekg(std::ios::end);
+    auto fs_end = fs.tellg();
+    fs.seekg(std::ios::beg);
+    auto fs_beg = fs.tellg();
+
+    auto file_size = static_cast<size_t>(fs_end - fs_beg);
+    auto vector_size = data.size();
+
+    data.reserve(vector_size + file_size);
+    data.insert(data.end(), std::istreambuf_iterator<char>(fs), std::istreambuf_iterator<char>());
+
+    fs.close();
+
+    return true;
+}
+
+bool _file_dump(const std::string &path, char *data, int size)
+{
+    std::fstream fs(path, std::ios::out | std::ios::binary);
+
+    if (!fs.is_open() || fs.fail())
+    {
+        fprintf(stderr, "[ERR] cannot open file %s \n", path.c_str());
+    }
+
+    fs.write(data, size);
+
+    return true;
+}
+
 static inline float sigmoid(float x)
 {
     return static_cast<float>(1.f / (1.f + exp(-x)));
@@ -149,6 +199,35 @@ int main(int argc, char *argv[])
     auto time_end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> diff = time_end - time_start;
     std::cout << "encode text Inference Cost time : " << diff.count() << "s" << std::endl;
+
+    // save text_features to bin file
+    for (size_t i = 0; i < texts.size(); i++)
+    {
+        std::string save_path = texts[i] + ".bin";
+        printf("{\n    \"test\": \"%s\",\n", texts[i].c_str());
+        printf("    \"texts_feature\": \"%s\",\n    \"input_ids\":[", save_path.c_str());
+        for (size_t j = 0; j < input_ids[i].size(); j++)
+        {
+            printf(" %ld", input_ids[i][j]);
+            if (j != input_ids[i].size() - 1)
+            {
+                printf(",");
+            }
+        }
+        printf("]\n}\n\n");
+
+        std::ofstream outfile;
+        outfile.open(save_path, std::ios::binary);
+        if (!outfile.good())
+        {
+            ALOGE("");
+            return -1;
+        }
+
+        outfile.write((const char *)text_features[i].data(), text_features[i].size() * sizeof(float));
+
+        outfile.close();
+    }
 
     std::vector<float> image_features;
     std::vector<cv::Rect2f> pred_boxes;
